8f37bc53e0b548951a83988b6771b72d2b5c5a74
ДМ. Па <qtckpuhdsa@gmail.com>
2020-08-27 13:47:35 +0300

    -

--- method_draft.py
+++ method_draft.py
@@ -7,6 +7,7 @@ Created on Sun Aug 23 15:17:26 2020
 
 import numpy as np
 from weeksum import get7sum
+from joblib import Parallel, delayed
 
 np.set_printoptions(precision=3, suppress = True)
 
@@ -252,7 +253,8 @@ def get_day_fullrandom2(foods, recipes, borders, mins, recipes_samples = 10, max
 
 
 def get_candidates(foods, recipes, borders, recipes_samples = 4, max_count = 3, tryes = 10, count = 100):
-    return [get_day_fullrandom(foods, recipes, borders, recipes_samples, max_count, tryes) for _ in range(count)]
+    #return [get_day_fullrandom(foods, recipes, borders, recipes_samples, max_count, tryes) for _ in range(count)]
+    return Parallel(n_jobs=6)(delayed(get_day_fullrandom)(foods, recipes, borders, recipes_samples, max_count, tryes) for _ in range(count))
 
 def get_optimal_candidates(foods, recipes, borders, recipes_samples = 4, max_count = 3, tryes = 10, count = 100, max_error_count = 3):
     cands = get_candidates(foods, recipes, borders, recipes_samples, max_count, tryes, count)
@@ -285,17 +287,35 @@ borders = pd.read_csv('currect_borders.csv').to_numpy()
 candidates = get_optimal_candidates(foods, recipes, borders, 4, 3, 10, 100, 3)
 
 
-recipes_count = np.arange(2, 9)
-max_count = np.arange(1, 4)
-tryes = np.arange(2, 15, 2)
+recipes_count = np.arange(2, 11)
+max_count = np.arange(1, 5)
+tryes = np.arange(2, 17, 2)
 R = np.empty((recipes_count.size, max_count.size, tryes.size))
 
-much = 200
+much = 300
 for i in range(R.shape[0]):
     for j in range(R.shape[1]):
         for k in range(R.shape[2]):
             R[i, j, k] = len(get_optimal_candidates(foods, recipes, borders, recipes_count[i], max_count[j], tryes[k], much, 3))/much
     print(R[i,:,:])
+
+
+
+import seaborn as sns
+import matplotlib.pylab as plt
+
+for i in range(len(recipes_count)):
+    ax = sns.heatmap(R[i,:,:], linewidth=0.5, vmin=R.min(), vmax=R.max(), annot = True, cmap = 'plasma')
+    ax.set_xticklabels(tryes)
+    ax.set_yticklabels(max_count)
+    plt.xlabel('Count of food attemps')
+    plt.ylabel('Maximum count of each recipe')
+    plt.title(f'Probs for {recipes_count[i]} recipes')
+    plt.savefig(f'./day_config_probs/recipes_count = {recipes_count[i]}.png', dpi = 300)
+    plt.close()
+    #plt.show()
+
+
 
 
 
