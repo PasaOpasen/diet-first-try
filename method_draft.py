63cca8683c164c56f179fe425928406eed09713c
ДМ. Па <qtckpuhdsa@gmail.com>
2020-08-27 15:38:54 +0300

    -

--- method_draft.py
+++ method_draft.py
@@ -287,33 +287,33 @@ borders = pd.read_csv('currect_borders.csv').to_numpy()
 candidates = get_optimal_candidates(foods, recipes, borders, 4, 3, 10, 100, 3)
 
 
-recipes_count = np.arange(2, 11)
-max_count = np.arange(1, 5)
-tryes = np.arange(2, 17, 2)
-R = np.empty((recipes_count.size, max_count.size, tryes.size))
+# recipes_count = np.arange(2, 11)
+# max_count = np.arange(1, 5)
+# tryes = np.arange(2, 17, 2)
+# R = np.empty((recipes_count.size, max_count.size, tryes.size))
 
-much = 300
-for i in range(R.shape[0]):
-    for j in range(R.shape[1]):
-        for k in range(R.shape[2]):
-            R[i, j, k] = len(get_optimal_candidates(foods, recipes, borders, recipes_count[i], max_count[j], tryes[k], much, 3))/much
-    print(R[i,:,:])
+# much = 400
+# for i in range(R.shape[0]):
+#     for j in range(R.shape[1]):
+#         for k in range(R.shape[2]):
+#             R[i, j, k] = len(get_optimal_candidates(foods, recipes, borders, recipes_count[i], max_count[j], tryes[k], much, 3))/much
+#     print(R[i,:,:])
 
 
 
-import seaborn as sns
-import matplotlib.pylab as plt
+# import seaborn as sns
+# import matplotlib.pylab as plt
 
-for i in range(len(recipes_count)):
-    ax = sns.heatmap(R[i,:,:], linewidth=0.5, vmin=R.min(), vmax=R.max(), annot = True, cmap = 'plasma')
-    ax.set_xticklabels(tryes)
-    ax.set_yticklabels(max_count)
-    plt.xlabel('Count of food attemps')
-    plt.ylabel('Maximum count of each recipe')
-    plt.title(f'Probs for {recipes_count[i]} recipes')
-    plt.savefig(f'./day_config_probs/recipes_count = {recipes_count[i]}.png', dpi = 300)
-    plt.close()
-    #plt.show()
+# for i in range(len(recipes_count)):
+#     ax = sns.heatmap(R[i,:,:], linewidth=0.5, vmin=R.min(), vmax=R.max(), annot = True, cmap = 'plasma')
+#     ax.set_xticklabels(tryes)
+#     ax.set_yticklabels(max_count)
+#     plt.xlabel('Count of food attemps')
+#     plt.ylabel('Maximum count of each recipe')
+#     plt.title(f'Probs for {recipes_count[i]} recipes')
+#     plt.savefig(f'./day_config_probs/recipes_count = {recipes_count[i]}.png', dpi = 300)
+#     plt.close()
+#     #plt.show()
 
 
 
