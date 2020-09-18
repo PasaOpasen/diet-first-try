b9fc6056ba09f9e9e4213a4e1613f11fa8c7d967
ДМ. Па <qtckpuhdsa@gmail.com>
2020-09-18 18:03:48 +0300

    -

--- split_by_sums.py
+++ split_by_sums.py
@@ -22,7 +22,7 @@ def get_sums_by_classes(vals, classes, classes_count = None):
     for v, c in zip(vals, classes):
         res[c] += v
     
-    return res
+    return {k: v for k, v in sorted(res.items(), key=lambda item: item[0])}
 
 def convert_sums_by_classes(dic, sums, tol = 10):
     res = defaultdict(float)
@@ -47,13 +47,13 @@ def get_current_dic(vals, classes, sums, tol = 10, classes_count = None):
 
 def which_max(vals, classes, class_index):
     
-    mx = float('inf')
+    mx = float('-inf')
     
     for i, (v, c) in enumerate(zip(vals, classes)):
         if c == class_index:
-            if v < mx:
+            if v > mx:
                 mx = v
-                result = v, i
+                result = (v, i)
     return result
 
 
@@ -72,20 +72,26 @@ def get_split_by_sums(vals, prefer_classes, sums, tol = 10):
     if flag:
         return result_classes, list(dic.values())   
     
-    procents = np.array(dic.values())
+    procents = np.array(list(dic.values()))
     
     while not flag:
         
+        print(procents)
+        
         max_class = np.argmax(procents)
         min_class = np.argmin(procents)
-        
+        print(f'{max_class} {min_class}')
         value, index = which_max(vals, result_classes, max_class)
         
-        procents[max_class] -= (value - sums[max_class])/sums[max_class]*100
-        procents[min_class] += (value - sums[min_class])/sums[min_class]*100
+        #procents[max_class] -= value/sums[max_class]*100
+        #procents[min_class] += value/sums[min_class]*100
+
         result_classes[index] = min_class
         
-        flag = np.sum(np.abs(procents) > tol) == 0
+        dic, flag = get_current_dic(vals, result_classes, sums, tol, sums.size)
+        procents = np.array(list(dic.values()))
+        
+        #flag = np.sum(np.abs(procents) > tol) == 0
     
     return result_classes, list(procents)
 
@@ -101,13 +107,14 @@ convert_sums_by_classes(d, [3.0,7.0,1.0])
 
 
 
-vals = np.random.uniform()
+vals = np.random.uniform(low = 0.01, high = 100, size = 50)
+classes = np.random.choice([0,1,2], 50, True)
+
+ans, p = get_split_by_sums(vals, classes, [0.25, 0.25, 0.5], tol = 10)
 
 
 
-
-
-
+get_current_dic(vals, ans, np.array([0.25, 0.25, 0.5]) * vals.sum(), tol = 10)
 
 
 
